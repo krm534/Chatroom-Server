97c20a16c0cc486be2fedcd630e29c3e3880b340
Kaleb Meeks <krmeeks15@gmail.com>
2022-11-15 22:01:33 -0600

    Added Log4J and logging statements. Also added google java formatter for more organized structure

--- src/main/java/Server/SendClient.java
+++ src/main/java/Server/SendClient.java
@@ -5,47 +5,49 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SendClient extends Thread {
-    private Server server;
-    private PrintWriter printWriter;
-    private Queue<String> messages;
+  private Server server;
+  private PrintWriter printWriter;
+  private Queue<String> messages;
 
-    public SendClient(Server server) {
-        this.messages = new LinkedList<>();
-        this.server = server;
-    }
+  private final Logger logger = Logger.getLogger(SendClient.class.getName());
 
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                if (messages.size() > 0) {
-                    System.out.println("Message size is greater than 0!");
-                    String clientMessage = messages.poll();
-                    printWriter.println(clientMessage);
-                    System.out.println("'" + clientMessage + "'" + " sent to clients!");
-                }
+  public SendClient(Server server) {
+    this.messages = new LinkedList<>();
+    this.server = server;
+  }
 
-                Thread.sleep(5000);
-            } catch (Exception e) {
-                System.out.println("Error Received: " + e.getMessage());
-                e.printStackTrace();
-            }
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        if (messages.size() > 0) {
+          logger.log(Level.INFO, "Message queue is greater than 0");
+          String clientMessage = messages.poll();
+          printWriter.println(clientMessage);
+          logger.log(Level.INFO, String.format("Message %s sent to all clients", clientMessage));
         }
-    }
 
-    public void setSocket(Socket socket) {
-        try {
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-        } catch (IOException e) {
-            System.out.println("Error Received: " + e.getMessage());
-            e.printStackTrace();
-        }
+        Thread.sleep(5000);
+      } catch (Exception e) {
+        logger.log(Level.SEVERE, "SendClient Exception: " + e.getMessage());
+      }
     }
+  }
 
-    public void appendToMessageQueue(String message) {
-        System.out.println(message + " placed in send queue!");
-        messages.add(message);
+  public void setSocket(Socket socket) {
+    try {
+      printWriter = new PrintWriter(socket.getOutputStream(), true);
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "SendClient Exception: " + e.getMessage());
     }
+  }
+
+  public void appendToMessageQueue(String message) {
+    messages.add(message);
+    logger.log(Level.INFO, message + " placed in send queue!");
+  }
 }
