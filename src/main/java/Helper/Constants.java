0dcfc9b9d1bd619ba438caa00ae9796458cc4778
Kaleb Meeks <krmeeks15@gmail.com>
2023-12-02 22:39:22 -0500

    Cleaned up and added more logging

---
+++ src/main/java/Helper/Constants.java
@@ -0,0 +1,5 @@
+package Helper;
+
+public class Constants {
+  public static final String DELIMITER = "DELIMITER";
+}
--- src/main/java/OutgoingResponseManager.java
+++ src/main/java/OutgoingResponseManager.java
@@ -1,37 +1,35 @@
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ClientSender extends Thread {
-  private ServerHandler serverHandler;
+public class OutgoingResponseManager extends Thread {
+  private ServerManager serverManager;
   private String message;
-  private final Logger LOGGER = Logger.getLogger(ClientSender.class.getName());
+  private final Logger LOGGER = LogManager.getLogger(OutgoingResponseManager.class.getName());
 
-  public ClientSender(ServerHandler serverHandler, String message) {
-    this.serverHandler = serverHandler;
+  public OutgoingResponseManager(ServerManager serverManager, String message) {
+    this.serverManager = serverManager;
     this.message = message;
   }
 
   @Override
   public void run() {
     try {
-      final List<Socket> sockets = serverHandler.getSockets();
-      LOGGER.log(
-          Level.INFO,
+      final List<Socket> sockets = serverManager.getSockets();
+      LOGGER.info(
           String.format("Total amount of sockets needing new message is %d", sockets.size()));
       for (Socket socket : sockets) {
         final PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
         printWriter.println(message);
-        LOGGER.log(
-            Level.INFO,
+        LOGGER.info(
             String.format(
                 "Message %s sent to client %s:%d",
                 message, socket.getInetAddress().getHostAddress(), socket.getPort()));
       }
     } catch (Exception e) {
-      LOGGER.log(Level.SEVERE, "SendClient Exception: " + e.getMessage());
+      LOGGER.error("Exception: " + e.getMessage());
     }
   }
 }
