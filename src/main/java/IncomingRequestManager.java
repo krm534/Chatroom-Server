6ac38356560f2390c0e4379470384c49f8098f57
Kaleb Meeks <krmeeks15@gmail.com>
2025-02-23 16:53:37 -0500

    feature/refactor: Udates for refactoring encryption handling and separating request/response threads

--- src/main/java/IncomingRequestManager.java
+++ src/main/java/IncomingRequestManager.java
@@ -6,8 +6,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Scanner;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,14 +25,10 @@ public class IncomingRequestManager extends Thread {
   private Gson gson;
 
   public IncomingRequestManager(int port, ServerManager serverManager) throws IOException {
-    createSocket(port);
+    this.serverSocket = new ServerSocket(port);
     this.serverHandler = serverManager;
     this.serverSocketPort = port;
     this.gson = new Gson();
-  }
-
-  private void createSocket(int port) throws IOException {
-    this.serverSocket = new ServerSocket(port);
   }
 
   @Override
@@ -36,36 +37,41 @@ public class IncomingRequestManager extends Thread {
       LOGGER.info("Listening for ServerSocket traffic on port " + serverSocket.getLocalPort());
       final Socket socket = serverSocket.accept();
       serverHandler.addSocket(socket, serverSocketPort);
-
       while (true) {
         final Scanner scanner = new Scanner(socket.getInputStream());
-
         final String input = scanner.nextLine();
         if (input.isEmpty()) {
           serverHandler.removeSocket(serverSocketPort, serverSocketPort);
           socket.close();
           break;
         }
-
-        final SocketController socketController =
-            serverHandler.searchSocketsByPort(serverSocketPort);
-        final SecretKey secretKey = socketController.getSecretKey();
-
-        final byte[] decodedMessage = Base64.getDecoder().decode(input);
-        final byte[] decryptedMessage = serverHandler.decryptMessage(decodedMessage, secretKey);
-        String decryptedMessageString = new String(decryptedMessage, StandardCharsets.UTF_8);
-        decryptedMessageString = decryptedMessageString.replace(Constants.DELIMITER, "");
-        LOGGER.info(String.format("Received message is %s", decryptedMessageString));
-
-        final Message message = gson.fromJson(decryptedMessageString, Message.class);
-        if (null == message.getMessage() || message.getMessage().isEmpty()) {
-          continue;
+        final byte[] decryptedMessage = receiveDecryptedMessage(input);
+        if (isReceivedMessageValid(decryptedMessage)) {
+          serverHandler.sendOutgoingMessage(decryptedMessage);
         }
-
-        serverHandler.sendOutgoingMessage(decryptedMessage);
       }
     } catch (Exception e) {
       LOGGER.error("Exception: " + e.getMessage());
     }
+  }
+
+  private byte[] receiveDecryptedMessage(String message)
+      throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+          BadPaddingException, InvalidKeyException {
+    final SocketController socketController = serverHandler.searchSocketsByPort(serverSocketPort);
+    final SecretKey secretKey = socketController.getSecretKey();
+    final byte[] decodedMessage = Base64.getDecoder().decode(message);
+    return EncryptionDecryptionHandler.decryptMessage(decodedMessage, secretKey);
+  }
+
+  private boolean isReceivedMessageValid(byte[] decryptedMessage) {
+    String decryptedMessageString = new String(decryptedMessage, StandardCharsets.UTF_8);
+    decryptedMessageString = decryptedMessageString.replace(Constants.DELIMITER, "");
+    LOGGER.info(String.format("Received message is %s", decryptedMessageString));
+    final Message message = gson.fromJson(decryptedMessageString, Message.class);
+    if (null == message.getMessage() || message.getMessage().isEmpty()) {
+      return false;
+    }
+    return true;
   }
 }
